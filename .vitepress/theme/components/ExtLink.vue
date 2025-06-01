62d2b7c39b95465d4868964f5029f7c22d3e412b
_Kerman <kermanx@qq.com>
2025-06-01 16:52:24 +0800

    chore: update golang

---
+++ .vitepress/theme/components/ExtLink.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+defineProps<{
+  href: string
+}>();
+</script>
+
+<template>
+  <a
+    :href="href"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <i class="i-mynaui-external-link inline-block w-0.8em h-0.8em" />
+    <slot />
+  </a>  
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,11 +3,12 @@ import DefaultTheme from 'vitepress/theme'
 import 'uno.css'
 import './style.css'
 import Layout from './Layout.vue'
+import ExtLink from './components/ExtLink.vue'
 
 export default {
   extends: DefaultTheme,
   Layout,
   enhanceApp({ app, router, siteData }) {
-    // ...
+    app.component('ExtLink', ExtLink)
   }
 } satisfies Theme
